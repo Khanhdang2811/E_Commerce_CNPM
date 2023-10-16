d54a8b80f0d3b28ab30182cd92cde3c102185ba2
Hoàng Thanh Huy <21dh110638@st.huflit.edu.vn>
2023-10-16 17:26:13 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import Cart from './customer/components/Cart/Cart';
+import Checkout from './customer/components/Checkout/Checkout';
 import Footer from './customer/components/Footer/Footer';
 import Navigation from './customer/components/Navigation/Navigation.jsx';
 import Product from './customer/components/Product/Product';
+import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 import HomePage from './customer/components/pages/HomePage/HomePage';
 
 function App() {
@@ -9,8 +12,11 @@ function App() {
     <div className="">
       <Navigation/>
       <div>
-        <Product/>
       {/* <HomePage/> */}
+       {/* <Products/> */}
+       {/*<ProductDetails/>*/}
+       {/*<Cart/>*/}
+       <Checkout/>
       </div>
       <Footer/>
     </div>
